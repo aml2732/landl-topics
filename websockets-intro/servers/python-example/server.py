2faccb15eca75bb4022125beb7ad1e038561743a
Angela LLoyd <angela.lloyd@move.com>
2023-07-07 17:26:55 -0500

    cleaned up python example a little bit

--- websockets-intro/servers/python-example/server.py
+++ websockets-intro/servers/python-example/server.py
@@ -1,10 +1,6 @@
 from flask import Flask
-import sys
 import asyncio
-import websockets
 from websockets.server import serve
-
-print("got here 1")
 
 app = Flask(__name__)
 listOfWebsockets = set()
@@ -13,15 +9,9 @@ listOfWebsockets = set()
 def rootPageRender():
     return "<p>Reached main page hosted via http</p>"
 
-async def messageHandler1(websocket):
-    listOfWebsockets.add(websocket)
-    async for message in websocket:
-        await websocket.send(message)
-
 async def messageHandler(websocket):
     listOfWebsockets.add(websocket)
     async for message in websocket:
-        print("message?", message)
         for ws in listOfWebsockets:
             await ws.send(message)
 
@@ -31,6 +21,5 @@ async def main():
 
 
 if __name__ == '__main__':
-    print("got here 12345", file=sys.stdout)
     asyncio.run(main())
     app.run(port=3000)
