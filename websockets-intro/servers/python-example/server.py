1011d9afb018446a22190d0993d488e5bca5277c
Angela LLoyd <angela.lloyd@move.com>
2023-07-21 17:23:02 -0500

    get python server to close when requested by client or on exit

--- websockets-intro/servers/python-example/server.py
+++ websockets-intro/servers/python-example/server.py
@@ -1,6 +1,7 @@
 from flask import Flask
 import asyncio
 from websockets.server import serve
+import atexit
 
 app = Flask(__name__)
 listOfWebsockets = set()
@@ -11,15 +12,25 @@ def rootPageRender():
 
 async def messageHandler(websocket):
     listOfWebsockets.add(websocket)
-    async for message in websocket:
-        for ws in listOfWebsockets:
-            await ws.send(message)
+    try:
+        async for message in websocket:
+            for ws in listOfWebsockets:
+                await ws.send(message)
+        await websocket.wait_closed()
+    finally:
+        listOfWebsockets.remove(websocket)
+
+
 
 async def main():
     async with serve(messageHandler, "localhost", 3000):
         await asyncio.Future()  # run forever
 
+async def closeAllClientConnections():
+    for ws in listOfWebsockets:
+        await ws.close()
 
 if __name__ == '__main__':
     asyncio.run(main())
+    atexit.register(closeAllClientConnections)
     app.run(port=3000)
